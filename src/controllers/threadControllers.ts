776ba5e8cfb2f2287c8668c260d03cff7772cadc
zero-id <abdullatiffebriliana15@gmail.com>
2024-06-08 19:05:12 +0700

    change link

--- src/controllers/threadControllers.ts
+++ src/controllers/threadControllers.ts
@@ -52,6 +52,16 @@ export default new (class ThreadControllers {
       return res.status(500).json({ error: error });
     }
   }
+
+  async delete(req: Request, res: Response) {
+    try {
+      const { id } = req.params;
+      await threadServices.delete(+id);
+      return res.status(200).json({ message: "success" });
+    } catch (error: any) {
+      return res.status(500).json({ error: error.message });
+    }
+  }
 })();
 // export const getThreads = async (req: Request, res: Response) => {
 //   try {
--- src/routes/threadRouter.ts
+++ src/routes/threadRouter.ts
@@ -17,5 +17,6 @@ threadRoute.post(
 threadRoute.get("/threads", threadControllers.findAll);
 threadRoute.get("/threads/replies/:id", threadControllers.findReplies);
 threadRoute.get("/threads/:id", threadControllers.findById);
+threadRoute.delete("/threads/:id", threadControllers.delete);
 
 export default threadRoute;
