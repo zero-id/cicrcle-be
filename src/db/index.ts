c3c50f25c8d823bb4166cd51e25df71db73d53d3
zero-id <abdullatiffebriliana15@gmail.com>
2024-06-08 20:51:14 +0700

    add feature delet threads

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,13 +1,6 @@
 import { PrismaClient, Prisma } from "@prisma/client";
-import { error } from "console";
 export * from "@prisma/client";
 
 const prisma = new PrismaClient();
-
-if (error instanceof Prisma.PrismaClientKnownRequestError) {
-    // error handling code
-    console.log('kontol');
-    
- }
 
 export default prisma;
